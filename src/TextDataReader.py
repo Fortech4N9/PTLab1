85c2d889f42c1b5a89dcf6cda37dfcf654233899
Valera Petrov <petrov@logema.org>
2024-12-02 14:09:06 +0300

    Доп задание, парсер JSON

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -2,12 +2,10 @@
 from src.Types import DataType
 from src.DataReader import DataReader
 
-
 class TextDataReader(DataReader):
     def __init__(self) -> None:
         self.key: str = ""
         self.students: DataType = {}
-
     def read(self, path: str) -> DataType:
         with open(path, encoding='utf-8') as file:
             for line in file:
@@ -16,6 +14,5 @@ class TextDataReader(DataReader):
                     self.students[self.key] = []
                 else:
                     subj, score = line.split(":", maxsplit=1)
-                    self.students[self.key].append(
-                        (subj.strip(), int(score.strip())))
+                    self.students[self.key].append((subj.strip(), int(score.strip())))
         return self.students
